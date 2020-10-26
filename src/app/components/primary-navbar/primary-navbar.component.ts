8d9cc5b75c3dca2516465ca731d06b25a2823fd2
Daniel Díaz <dalejodc@gmail.com>
2020-10-25 19:09:19 -0600

    feat(layout): add navbar animation

--- src/app/components/primary-navbar/primary-navbar.component.ts
+++ src/app/components/primary-navbar/primary-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-primary-navbar',
@@ -9,7 +9,22 @@ export class PrimaryNavbarComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild('mySidenav') el: ElementRef;
+
   ngOnInit(): void {
   }
 
+  openNav() {
+    const navbar = 80;
+    const sidebarWidth = navbar + ((window.innerWidth - (navbar * 2)) / 2);
+    this.el.nativeElement.style.zIndex = 1;
+    this.el.nativeElement.style.visibility = 'visible';
+    this.el.nativeElement.style.width = `${sidebarWidth}px`;
+  }
+
+  closeNav() {
+    this.el.nativeElement.style.width = "0";
+    this.el.nativeElement.style.zIndex = -1;
+    this.el.nativeElement.style.visibility = 'hidden';
+  }
 }
